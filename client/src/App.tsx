49099a097e1b4cfc9dfbf501b3c2c8451e914744
Prasanth-S7 <prasanthsampath2005@gmail.com>
2025-03-17 01:08:03 +0530

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,10 @@ import { useState } from "react"
 function App() {
   const [email, setEmail] = useState("")
 
-  const submitCertificateReq = async () => {
-    try {
-      const res = await redisClient.set(`email:${email}`, "true")
-      console.log(res)
-    }
-    catch(error){
-      console.error(error)
-    }
-  }
-
   return (
     <div>
       <input type="text" onChange={(e) => setEmail(e.target.value)} />
-      <button onClick={submitCertificateReq}>Generate Certificate</button>
+      <button>Generate Certificate</button>
     </div>
   )
 }
