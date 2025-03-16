4934ccf14bb25fa7726590cbc57026a12b5cfe62
Prasanth-S7 <prasanthsampath2005@gmail.com>
2025-03-17 01:09:34 +0530

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react"
-
 function App() {
-  const [email, setEmail] = useState("")
 
   return (
     <div>
-      <input type="text" onChange={(e) => setEmail(e.target.value)} />
+      <input type="text" />
       <button>Generate Certificate</button>
     </div>
   )
